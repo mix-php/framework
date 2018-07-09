6eaed3cfeecf312d5307a0d1b08c7d46cd840c08
Jian Liu <onanying@126.com>
2018-07-09 19:41:26 +0800

    增加众多功能

--- src/client/Http.php
+++ src/client/Http.php
@@ -17,71 +17,184 @@ class Http extends BaseObject
     // 请求头
     public $headers;
 
-    // 上次请求的返回包体
+    // cookies
+    public $cookies;
+
+    // 请求 URL
+    protected $_requestUrl;
+
+    // 请求方法
+    protected $_requestMethod;
+
+    // 请求头
+    protected $_requestHeaders;
+
+    // 请求包体
+    protected $_requestBody;
+
+    // 响应头
+    protected $_headers;
+
+    // 响应包体
     protected $_body;
 
-    // 上次请求的返回的Http状态码
+    // Http状态码
     protected $_statusCode;
 
-    // 上次请求的错误信息
+    // 错误信息
     protected $_error;
 
-    // GET请求
+    // 设置请求头
+    public function setHeader($key, $value)
+    {
+        $this->headers[$key] = $value;
+    }
+
+    // 设置 Cookie
+    public function setCookie($key, $value)
+    {
+        $this->cookies[$key] = $value;
+    }
+
+    // GET 请求
     public function get($url)
     {
-        $this->execute($url, 'GET');
-        return $this;
+        return $this->request($url, 'GET');
     }
 
-    // POST请求
-    public function post($url, $requestBody)
+    // POST 请求
+    public function post($url, $body)
     {
-        $this->execute($url, 'POST', $requestBody);
-        return $this;
+        return $this->request($url, 'POST', $body);
     }
 
-    // 执行
-    protected function execute($url, $requestType, $requestBody = null)
+    // 执行请求
+    public function request($url, $method, $body = null)
     {
-        // 转换请求包体
-        if (is_array($requestBody)) {
-            $requestBody = http_build_query($requestBody);
-        }
+        // 定义变量
+        $this->_requestUrl    = $url;
+        $this->_requestMethod = $method;
+        $requestHeaders       = self::buildRequestHeaders($this->headers);
+        $requestCookies       = self::buildRequestCookies($this->cookies);
+        $this->_requestBody   = self::buildRequestBody($body);
         // 构造请求
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $requestType);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
         isset($this->timeout) and curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, isset($this->headers) ? $this->headers : []);
-        isset($requestBody) and curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
-        // 忽略SSL
+        curl_setopt($ch, CURLOPT_URL, $this->_requestUrl);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $this->_requestMethod);
+        empty($requestHeaders) or curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
+        empty($requestCookies) or curl_setopt($ch, CURLOPT_COOKIE, $requestCookies);
+        empty($this->_requestBody) or curl_setopt($ch, CURLOPT_POSTFIELDS, $this->_requestBody);
+        // 忽略 SSL
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        // 返回数据
-        $this->_body       = curl_exec($ch);
-        $this->_error      = curl_error($ch);
-        $this->_statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        // 响应数据
+        $response              = curl_exec($ch);
+        $headerSize            = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $this->_error          = curl_error($ch);
+        $this->_requestHeaders = trim(curl_getinfo($ch)['request_header']);
+        $this->_statusCode     = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $this->_headers        = trim(substr($response, 0, $headerSize));
+        $this->_body           = substr($response, $headerSize);
         // 关闭请求
         curl_close($ch);
+        // 返回
+        return empty($this->_error) ? true : false;
     }
 
-    // 返回上次请求的包体
+    // 返回请求 URL
+    public function getRequestUrl()
+    {
+        return $this->_requestUrl;
+    }
+
+    // 返回请求方法
+    public function getRequestMethod()
+    {
+        return $this->_requestMethod;
+    }
+
+    // 返回响应头
+    public function getRequestHeaders()
+    {
+        return self::headersStringToArray($this->_requestHeaders);
+    }
+
+    // 返回响应包体
+    public function getRequestBody()
+    {
+        return $this->_requestBody;
+    }
+
+    // 返回响应头
+    public function getHeaders()
+    {
+        return self::headersStringToArray($this->_headers);
+    }
+
+    // 返回响应包体
     public function getBody()
     {
         return $this->_body;
     }
 
-    // 返回上次请求的Http状态码
+    // 返回Http状态码
     public function getStatusCode()
     {
         return $this->_statusCode;
     }
 
-    // 返回上次请求的错误信息
+    // 返回错误信息
     public function getError()
     {
         return $this->_error;
+    }
+
+    // 构建请求正文
+    protected static function buildRequestBody($data)
+    {
+        if (empty($data)) {
+            return '';
+        }
+        return http_build_query($data);
+    }
+
+    // 构建请求头
+    protected static function buildRequestHeaders($data)
+    {
+        $headers = [];
+        if (empty($data)) {
+            return $headers;
+        }
+        foreach ($data as $key => $value) {
+            $headers[] = "{$key}: {$value}";
+        }
+        return $headers;
+    }
+
+    // 构建请求的 Cookies
+    protected static function buildRequestCookies($data)
+    {
+        if (empty($data)) {
+            return '';
+        }
+        return http_build_query($data, '', ';');
+    }
+
+    // 头信息字符转数组
+    protected static function headersStringToArray($headersString)
+    {
+        $headersString = explode("\n", $headersString);
+        array_shift($headersString);
+        $headerArray = [];
+        foreach ($headersString as $header) {
+            list($key, $value) = explode(':', $header);
+            $headerArray[$key] = trim($value);
+        }
+        return $headerArray;
     }
 
 }

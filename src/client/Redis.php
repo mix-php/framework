69c9cfbaef31afe6c4d8bb84e62b9e008da1d1db
Jian Liu <onanying@126.com>
2018-09-04 15:44:54 +0800

    修改注释位置

--- src/client/Redis.php
+++ src/client/Redis.php
@@ -5,32 +5,6 @@ namespace mix\client;
 /**
  * redis组件
  * @author 刘健 <coder.liu@qq.com>
- *
- * @method select($index)
- * @method set($key, $value)
- * @method setex($key, $seconds, $value)
- * @method setnx($key, $value)
- * @method get($key)
- * @method del($key)
- * @method hmset($key, $array)
- * @method hmget($key, $array)
- * @method hgetall($key)
- * @method hlen($key)
- * @method hset($key, $field, $value)
- * @method hsetnx($key, $field, $value)
- * @method hget($key, $field)
- * @method lpush($key, $value)
- * @method rpop($key)
- * @method brpop($key, $timeout)
- * @method rpush($key, $value)
- * @method lpop($key)
- * @method blpop($key, $timeout)
- * @method sadd($key, $value)
- * @method lrange($key, $start, $end)
- * @method llen($key)
- * @method subscribe($channel)
- * @method publish($channel, $message)
- * @method ttl($key)
  */
 class Redis extends BaseRedis
 {

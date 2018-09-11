920e6ac068de20dcde363d9d50ca5deb2ca0a8be
Jian Liu <onanying@126.com>
2018-09-11 17:41:33 +0800

    去掉parent

--- src/base/StaticInstanceInterface.php
+++ src/base/StaticInstanceInterface.php
@@ -9,16 +9,11 @@ namespace mix\base;
 interface StaticInstanceInterface
 {
 
-    // 配置类型值
-    const CONFIG_COMPONENTS = 'components';
-    const CONFIG_LIBRARIES = 'libraries';
-
     /**
      * 创建实例，通过配置名
-     * @param null $name
-     * @param string $parent
+     * @param $name
      * @return $this
      */
-    public static function newInstanceByConfig($name = null, $parent = StaticInstanceInterface::CONFIG_LIBRARIES);
+    public static function newInstanceByConfig($name);
 
 }
--- src/base/StaticInstanceTrait.php
+++ src/base/StaticInstanceTrait.php
@@ -11,14 +11,13 @@ trait StaticInstanceTrait
 
     /**
      * 创建实例，通过配置名
-     * @param null $name
-     * @param string $parent
+     * @param $name
      * @return $this
      */
-    public static function newInstanceByConfig($name = null, $parent = StaticInstanceInterface::CONFIG_LIBRARIES)
+    public static function newInstanceByConfig($name)
     {
         $class  = get_called_class();
-        $config = app()->config("{$parent}.[{$name}]");
+        $config = app()->config($name);
         $object = create_object($config);
         if (get_class($object) != $class) {
             throw new \ConfigException('实例化类型与配置类型不符');

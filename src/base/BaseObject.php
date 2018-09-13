ad7160e429ff22dbbce83eb3b76cdd762c3a101e
Jian Liu <onanying@126.com>
2018-09-13 16:07:39 +0800

    为了ide自动补全友好，冗余部分方法。

--- src/base/BaseObject.php
+++ src/base/BaseObject.php
@@ -9,6 +9,38 @@ namespace mix\base;
 abstract class BaseObject implements BaseObjectInterface, StaticInstanceInterface
 {
 
-    use BaseObjectTrait, StaticInstanceTrait;
+    use StaticInstanceTrait;
+
+    // 构造
+    public function __construct($config = [])
+    {
+        // 执行构造事件
+        $this->onConstruct();
+        // 导入属性
+        \Mix::configure($this, $config);
+        // 执行初始化事件
+        $this->onInitialize();
+    }
+
+    // 析构
+    public function __destruct()
+    {
+        $this->onDestruct();
+    }
+
+    // 构造事件
+    public function onConstruct()
+    {
+    }
+
+    // 初始化事件
+    public function onInitialize()
+    {
+    }
+
+    // 析构事件
+    public function onDestruct()
+    {
+    }
 
 }

5c1be4b8c3385bea4d23a5b0dd4f2407c145954b
Jian Liu <onanying@126.com>
2018-11-06 15:29:36 +0800

    增加容器类

---
+++ src/Base/Container.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mix\Base;
+
+/**
+ * 容器类
+ * @author 刘健 <coder.liu@qq.com>
+ */
+class Container
+{
+
+
+    public function get($id)
+    {
+
+    }
+
+
+    public function has($id){
+
+    }
+
+}

9d5adc508a1c77da7e157e450b54aaa817856548
Jian Liu <onanying@126.com>
2018-11-30 15:27:32 +0800

    增加抽象类，开放组件注释给用户

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -3,29 +3,13 @@
 namespace Mix\Core;
 
 use Mix\Container\Container;
+use Apps\Common\Core\ApplicationInterface;
 
 /**
  * App类
  * @author 刘健 <coder.liu@qq.com>
- *
- * @property \Mix\Log\Logger $log
- * @property \Mix\Console\Input $input
- * @property \Mix\Console\Output $output
- * @property \Mix\Http\Route $route
- * @property \Mix\Http\Request|\Mix\Http\Compatible\Request $request
- * @property \Mix\Http\Response|\Mix\Http\Compatible\Response $response
- * @property \Mix\Http\Error|\Mix\Console\Error $error
- * @property \Mix\Http\Token $token
- * @property \Mix\Http\Session $session
- * @property \Mix\Http\Cookie $cookie
- * @property \Mix\Database\PDOConnection $pdo
- * @property \Mix\Redis\RedisConnection $redis
- * @property \Mix\WebSocket\TokenReader $tokenReader
- * @property \Mix\WebSocket\SessionReader $sessionReader
- * @property \Mix\WebSocket\MessageHandler $messageHandler
- * @property \Mix\Pool\ConnectionPool $connectionPool
  */
-class Application extends BaseObject
+class Application extends BaseObject implements ApplicationInterface
 {
 
     // 初始化回调

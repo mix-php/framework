4cd660e15879a024d02d4bfdb12725d3ddef0a0f
Jian Liu <onanying@126.com>
2018-12-04 17:54:58 +0800

    修复多次引用导致的析构回收问题

--- src/Pool/ConnectionPool.php
+++ src/Pool/ConnectionPool.php
@@ -25,7 +25,7 @@ class ConnectionPool extends Component
     protected $_queue;
 
     // 活跃连接集合
-    protected $_actives = [];
+    protected $_actives;
 
     // 初始化事件
     public function onInitialize()
@@ -38,43 +38,39 @@ class ConnectionPool extends Component
     // 获取连接
     public function getConnection()
     {
-        // 队列有连接，从队列取
-        if ($this->getQueueCount() > 0) {
-            return $this->pop();
+        if ($this->getIdleNumber() > 0 || $this->getTotalNumber() >= $this->maxActive) {
+            // 队列有连接，从队列取
+            // 达到最大连接数，从队列取
+            $connection = $this->pop();
+        } else {
+            // 创建连接
+            $connection = $this->createConnection();
         }
-        // 达到最大连接数，从队列取
-        if ($this->getTotalCount() >= $this->maxActive) {
-            return $this->pop();
-        }
-        // 创建连接
-        $connection          = $this->createConnection();
-        $id                  = spl_object_hash($connection);
-        $this->_actives[$id] = $connection;
+        $this->_actives++;
         return $connection;
     }
 
     // 释放连接
     public function release($connection)
     {
-        $id = spl_object_hash($connection);
         $this->push($connection);
-        unset($this->_actives[$id]);
+        $this->_actives--;
     }
 
     // 获取连接池的统计信息
     public function getStats()
     {
         return [
-            'total'  => $this->getTotalCount(),
-            'queue'  => $this->getQueueCount(),
-            'active' => $this->getActiveCount(),
+            'total'  => $this->getTotalNumber(),
+            'idle'   => $this->getIdleNumber(),
+            'active' => $this->getActiveNumber(),
         ];
     }
 
     // 放入连接
     protected function push($connection)
     {
-        if ($this->getQueueCount() < $this->maxIdle) {
+        if ($this->getIdleNumber() < $this->maxIdle) {
             return $this->_queue->push($connection);
         }
         return false;
@@ -87,22 +83,22 @@ class ConnectionPool extends Component
     }
 
     // 获取队列中的连接数
-    protected function getQueueCount()
+    protected function getIdleNumber()
     {
         $count = $this->_queue->stats()['queue_num'];
         return $count < 0 ? 0 : $count;
     }
 
     // 获取活跃的连接数
-    protected function getActiveCount()
+    protected function getActiveNumber()
     {
-        return count($this->_actives);
+        return $this->_actives;
     }
 
     // 获取当前总连接数
-    protected function getTotalCount()
+    protected function getTotalNumber()
     {
-        return $this->getQueueCount() + $this->getActiveCount();
+        return $this->getIdleNumber() + $this->getActiveNumber();
     }
 
 }

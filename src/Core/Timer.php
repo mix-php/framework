a6a78a3123b48c377c4e6bce9bec2d959c1d5c09
Jian Liu <onanying@126.com>
2018-12-24 15:12:54 +0800

    新增定时器封装

---
+++ src/Core/Timer.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Mix\Core;
+
+/**
+ * Class Timer
+ * @package Mix\Core
+ */
+class Timer
+{
+
+    /**
+     * 定时器ID
+     * @var int
+     */
+    protected $_timerId;
+
+    /**
+     * 在指定的时间后执行函数
+     * 一次性执行
+     * @param int $msec
+     * @param callable $callback
+     */
+    public static function after(int $msec, callable $callback)
+    {
+        return swoole_timer_after($msec, function () use ($callback) {
+            // 执行闭包
+            try {
+                call_user_func($callback);
+            } catch (\Throwable $e) {
+                // 输出错误
+                \Mix::$app->error->handleException($e);
+            }
+        });
+    }
+
+    /**
+     * 设置一个间隔时钟定时器
+     * 持续触发
+     * @param int $msec
+     * @param callable $callback
+     */
+    public function tick(int $msec, callable $callback)
+    {
+        // 清除旧定时器
+        $this->clear();
+        // 设置定时器
+        $timerId = swoole_timer_tick($msec, function () use ($callback) {
+            // 执行闭包
+            try {
+                call_user_func($callback);
+            } catch (\Throwable $e) {
+                // 输出错误
+                \Mix::$app->error->handleException($e);
+            }
+        });
+        // 保存id
+        $this->_timerId = $timerId;
+        // 返回
+        return $timerId;
+    }
+
+    /**
+     * 清除旧定时器
+     * @return bool
+     */
+    public function clear()
+    {
+        if (isset($this->_timerId)) {
+            return swoole_timer_clear($this->_timerId);
+        }
+        return false;
+    }
+
+}

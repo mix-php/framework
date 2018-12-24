288f5a2d4d3f345eed2cf114988eb67bfd314cd1
Jian Liu <onanying@126.com>
2018-12-24 12:15:56 +0800

    增加注释

--- src/Core/Channel.php
+++ src/Core/Channel.php
@@ -5,6 +5,14 @@ namespace Mix\Core;
 /**
  * 通道类
  * @author LIUJIAN <coder.keda@gmail.com>
+ *
+ * @method bool push($data)
+ * @method mixed pop()
+ * @method bool isEmpty()
+ * @method bool isFull()
+ * @method array stats()
+ * @method int length()
+ * @method close()
  */
 class Channel extends \Swoole\Coroutine\Channel
 {

82fd2ba7144fb3edbfc41792dcb48a1fd65f987e
Jian Liu <onanying@126.com>
2019-01-09 16:32:52 +0800

    连接池改造

--- src/Pool/ConnectionPoolInterface.php
+++ src/Pool/ConnectionPoolInterface.php
@@ -5,20 +5,33 @@ namespace Mix\Pool;
 /**
  * Interface ConnectionPoolInterface
  * @author LIUJIAN <coder.keda@gmail.com>
+ * @package Mix\Pool
  */
 interface ConnectionPoolInterface
 {
 
-    // 创建连接
+    /**
+     * 创建连接
+     * @return mixed
+     */
     public function createConnection();
 
-    // 获取连接
+    /**
+     * 获取连接
+     * @return mixed
+     */
     public function getConnection();
 
-    // 释放连接
+    /**
+     * 释放连接
+     * @param $connection
+     */
     public function release($connection);
 
-    // 获取连接池的统计信息
+    /**
+     * 获取连接池的统计信息
+     * @return array
+     */
     public function getStats();
 
 }

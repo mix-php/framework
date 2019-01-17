8eedff65dc42306df7366b3fa69aaf84aac9b795
Jian Liu <onanying@126.com>
2019-01-17 14:52:08 +0800

    优化特殊错误的处理

--- src/Core/Error.php
+++ src/Core/Error.php
@@ -44,6 +44,11 @@ class Error
     {
         if (error_reporting() & $errno) {
             // 委托给异常处理
+            if (self::isFatalWarning($errno, $errstr)) {
+                self::appException(new \Mix\Exceptions\ErrorException($errno, $errstr, $errfile, $errline));
+                return;
+            }
+            // 转换为异常抛出
             throw new \Mix\Exceptions\ErrorException($errno, $errstr, $errfile, $errline);
         }
     }
@@ -128,6 +133,21 @@ class Error
     public static function isFatal($errno)
     {
         return in_array($errno, [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE]);
+    }
+
+    /**
+     * 是否致命警告类型
+     * 特殊的警告，出现后 try/catch 将无法捕获异常。
+     * @param $errno
+     * @param $errstr
+     * @return bool
+     */
+    public static function isFatalWarning($errno, $errstr)
+    {
+        if ($errno == E_WARNING && strpos($errstr, 'require') === 0) {
+            return true;
+        }
+        return false;
     }
 
 }

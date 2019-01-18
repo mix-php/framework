0217b1278c4806cdea1b9b66814353d30c79f2f1
Jian Liu <onanying@126.com>
2019-01-18 13:43:24 +0800

    增加错误级别可多次配置

--- src/Core/Error.php
+++ src/Core/Error.php
@@ -20,14 +20,17 @@ class Error
      */
     public static function register()
     {
+        // 设置错误级别
+        $level = \Mix::$app->error->level;
+        if (error_reporting() !== $level) {
+            error_reporting($level);
+        }
         // 多次注册处理
         if (self::$registered) {
             return;
         }
         self::$registered = true;
         // 注册错误处理
-        $level = \Mix::$app->error->level;
-        error_reporting($level);
         set_error_handler([__CLASS__, 'appError']);
         set_exception_handler([__CLASS__, 'appException']); // swoole 不支持该函数
         register_shutdown_function([__CLASS__, 'appShutdown']);

ca6c23e53c393b034dbffad4635b7d4b143d2a21
Jian Liu <onanying@126.com>
2019-03-08 17:37:57 +0800

    更新

--- src/Core/Bean/AbstractObject.php
+++ src/Core/Bean/AbstractObject.php
@@ -9,7 +9,68 @@ namespace Mix\Core\Bean;
  */
 abstract class AbstractObject implements ObjectInterface
 {
+    
+    /**
+     * 构造
+     * BeanObject constructor.
+     * @param array $config
+     */
+    public function __construct($config = [])
+    {
+        // 执行构造事件
+        $this->onConstruct();
+        // 构建配置
+        $config = \Mix::configure($this, $config);
+        // 导入属性
+        \Mix::importProperties($this, $config);
+        // 执行初始化事件
+        $this->onInitialize();
+    }
 
-    use ObjectTrait;
+    /**
+     * 析构
+     */
+    public function __destruct()
+    {
+        $this->onDestruct();
+    }
+
+    /**
+     * 构造事件
+     */
+    public function onConstruct()
+    {
+    }
+
+    /**
+     * 初始化事件
+     */
+    public function onInitialize()
+    {
+    }
+
+    /**
+     * 析构事件
+     */
+    public function onDestruct()
+    {
+    }
+
+    /**
+     * 使用依赖创建实例
+     * @param $name
+     * @return $this
+     */
+    public static function newInstance($name = null)
+    {
+        $currentClass = get_called_class();
+        $bean         = Bean::config(is_null($name) ? Bean::name($currentClass) : $name);
+        $class        = $bean['class'];
+        $properties   = $bean['properties'] ?? [];
+        if ($class != $currentClass) {
+            throw new \Mix\Exception\ConfigException("Bean class is not equal to the current class, Current class: {$currentClass}, Bean class: {$class}");
+        }
+        return new $class($properties);
+    }
 
 }
--- src/Core/Component/AbstractComponent.php
+++ src/Core/Component/AbstractComponent.php
@@ -11,7 +11,51 @@ use Mix\Core\Bean\AbstractObject;
  */
 abstract class AbstractComponent extends AbstractObject implements ComponentInterface
 {
+    
+    /**
+     * 协程模式
+     * @var int
+     */
+    public static $coroutineMode = ComponentInterface::COROUTINE_MODE_NEW;
 
-    use ComponentTrait;
+    /**
+     * 组件状态
+     * @var int
+     */
+    private $_status = ComponentInterface::STATUS_READY;
+
+    /**
+     * 获取组件状态
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->_status;
+    }
+
+    /**
+     * 设置组件状态
+     * @param int $status
+     */
+    public function setStatus(int $status)
+    {
+        $this->_status = $status;
+    }
+
+    /**
+     * 前置处理事件
+     */
+    public function onBeforeInitialize()
+    {
+        $this->setStatus(ComponentInterface::STATUS_RUNNING);
+    }
+
+    /**
+     * 后置处理事件
+     */
+    public function onAfterInitialize()
+    {
+        $this->setStatus(ComponentInterface::STATUS_READY);
+    }
 
 }
